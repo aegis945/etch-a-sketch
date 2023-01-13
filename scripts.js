a4e7698b3b3d7e068de6793b1df6c53adb2fab76
aegis945 <artiom.lekincev@gmail.com>
2023-01-14 01:44:56 +0200

    making the code more readable

--- scripts.js
+++ scripts.js
@@ -1,16 +1,27 @@
 let gridSize = 16;
+
+function main() {
+    createGrid(gridSize);
+    updateGrid();
+    changeColor();
+    clearGridSquare();
+}
+
 /*display slider value*/
 const slider = document.getElementById("myRange");
 const output = document.getElementById("demo");
 output.innerHTML = slider.value + "x" + slider.value;
+
 /*update grid size according to slider value*/
-slider.addEventListener("input", function () {
-    gridSize = slider.value;
-    output.innerHTML = slider.value + "x" + slider.value;;
-    clearGrid();
-    createGrid(gridSize);
-    changeColor();
-});
+function updateGrid() {
+    slider.addEventListener("input", function () {
+        gridSize = slider.value;
+        output.innerHTML = slider.value + "x" + slider.value;;
+        clearGrid();
+        createGrid(gridSize);
+        changeColor();
+    });
+}
 
 function createGrid(gridSize) {
     const container = document.querySelector(".grid-container");
@@ -24,23 +35,11 @@ function createGrid(gridSize) {
         }
     }
 }
-createGrid(gridSize);
-changeColor();
-clearGridSquare();
 
 function clearGrid() {
     const gridElements = document.querySelectorAll(".grid-square");
     for (let i = 0; (el = gridElements[i]); i++) {
         el.parentNode.removeChild(el);
-    }
-}
-
-function changeColor() {
-    const gridElements = document.querySelectorAll(".grid-square");
-    for (let i = 0; (el = gridElements[i]); i++) {
-        el.addEventListener("click", function () {
-            gridElements[i].style.backgroundColor = "black";
-        });
     }
 }
 
@@ -53,3 +52,14 @@ function clearGridSquare() {
         });
     }
 }
+
+function changeColor() {
+    const gridElements = document.querySelectorAll(".grid-square");
+    for (let i = 0; (el = gridElements[i]); i++) {
+        el.addEventListener("click", function () {
+            gridElements[i].style.backgroundColor = "black";
+        });
+    }
+}
+
+main();
