7a852f611945757c81f1ad6a6ffa8c5275f1daad
aegis945 <artiom.lekincev@gmail.com>
2023-01-14 01:26:54 +0200

    added function that allows to draw on a grid

--- scripts.js
+++ scripts.js
@@ -2,13 +2,14 @@ let gridSize = 16;
 /*display slider value*/
 const slider = document.getElementById("myRange");
 const output = document.getElementById("demo");
-output.innerHTML = slider.value;
+output.innerHTML = slider.value + "x" + slider.value;
 /*update grid size according to slider value*/
-slider.addEventListener("mouseup", function () {
+slider.addEventListener("input", function () {
     gridSize = slider.value;
-    output.innerHTML = slider.value;
+    output.innerHTML = slider.value + "x" + slider.value;;
     clearGrid();
     createGrid(gridSize);
+    changeColor();
 });
 
 function createGrid(gridSize) {
@@ -23,12 +24,21 @@ function createGrid(gridSize) {
         }
     }
 }
+createGrid(gridSize);
+changeColor();
 
 function clearGrid() {
-    let gridElements = document.querySelectorAll(".grid-square");
+    const gridElements = document.querySelectorAll(".grid-square");
     for (let i = 0; (el = gridElements[i]); i++) {
         el.parentNode.removeChild(el);
     }
 }
 
-createGrid(gridSize);
+function changeColor() {
+    const gridElements = document.querySelectorAll(".grid-square");
+    for (let i = 0; (el = gridElements[i]); i++) {
+        el.addEventListener("click", function () {
+            gridElements[i].style.backgroundColor = "black";
+        });
+    }
+}
